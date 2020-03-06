9f94987abd9207a2bd8c43f7c92440fc663209c7
92Reza <reza.ghari1995@gmail.com>
2020-03-07 01:17:21 +0330

    Customizing the admin panel apearance with css.

--- btre/settings.py
+++ btre/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import config
+from btre import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre/static')
 ]
+# Media Folder Settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- listings/models.py
+++ listings/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from realtors.models import Realtor
 
 # Create your models here.
-class listing(models.Model):
+class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
