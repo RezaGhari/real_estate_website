22f0d213bf924774e3fd76f77e966a1e95a34c9d
92Reza <reza.ghari1995@gmail.com>
2020-03-06 05:41:49 +0330

    Add two apps: realtors and listings

--- btre/urls.py
+++ btre/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('listings/', include('listings.urls')),
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
