8df442f8ab45656134b2275b57c2f3f003ee6d0c
92Reza <reza.ghari1995@gmail.com>
2020-03-07 02:33:23 +0330

    Customizing Admin Area and the columns it'll  show.

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import Realtor
 # Register your models here.
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email',
+    'hire_date', 'is_mvp', 'photo')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+    list_editable = ('is_mvp',)
+
+admin.site.register(Realtor, RealtorAdmin)
