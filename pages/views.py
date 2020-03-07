69b49850c0dd06e0ab44044d02cdd5162cf56591
92Reza <55861777+92reza@users.noreply.github.com>
2020-03-07 18:17:21 +0330

    Adding backend search data for search fields.

--- pages/views.py
+++ pages/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 # Create your views here.
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices,state_choices
 
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     
     context = {
-        'listings': listings
+        'listings': listings,
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'state_choices':state_choices
     }
     return render(request, "pages/index.html", context)
 
