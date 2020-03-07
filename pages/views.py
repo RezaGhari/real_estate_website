dfc6b22f5cd0923c4db3b7aa584d5273177b0774
92Reza <reza.ghari1995@gmail.com>
2020-03-07 05:31:59 +0330

    Adding dynamic data to About page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 # Create your views here.
 from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -12,4 +14,10 @@ def index(request):
 
 
 def about(request):
-    return render(request, "pages/about.html")
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+    context = {
+        'realtors': realtors,
+        'mvp_realtors':mvp_realtors
+    }
+    return render(request, "pages/about.html", context)
