4a35212a44df90a7d82a0d93c024734cc60d2536
92Reza <reza.ghari1995@gmail.com>
2020-03-09 19:16:39 +0330

    Backend for submission of inquiry and auto-sending emails to realtors

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# email config
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config.email_host_user
+EMAIL_HOST_PASSWORD = config.email_host_password
+EMAIL_USE_TLS=True
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.mail import send_mail
+from .models import Contact
+from btre import config
 # Create your views here.
 def contact(request):
-    return 
+    if request.method == "POST":
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        realtor_email = request.POST['realtor_email']
+
+        contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                          email=email, phone=phone, message=message, user_id=user_id)
+
+        contact.save()
+
+        send_mail(
+            'Property Listing Inquiry',
+            'There has been an inquiry for'+listing+'. Sign into the admin panel for more info',
+            config.email_host_user,
+            [realtor_email, 'reza.ghari@yahoo.com'],
+            fail_silently=False
+        )
+
+        messages.success(request, "Your request has been submitted, a realtor will get back to you soon!")
+        return redirect('/listings/'+listing_id)
