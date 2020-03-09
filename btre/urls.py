c4a613ed2ad800f68d19caaa009c9b2e6fdfb88b
92Reza <reza.ghari1995@gmail.com>
2020-03-09 06:16:55 +0330

    Backend for making inquiry for a house

--- btre/urls.py
+++ btre/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('listings/', include('listings.urls')),
     path('', include('pages.urls')),
-    path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),
     path('account/', include('account.urls')),
+    path('contacts/', include('contacts.urls')),
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
